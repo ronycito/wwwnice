534d7cb5cddeec421ebe0859eafbac96444853f1
ronycito <manuelbarandicaqwe@gmail.com>
2022-10-12 15:33:51 -0500

    Updates

---
+++ precache-manifest.d6b6ea79a66438f39921d49d0661bedb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a82f7ad6dad066a66bdb",
+    "url": "/wwwnice/static/css/main.60083aa0.chunk.css"
+  },
+  {
+    "revision": "a82f7ad6dad066a66bdb",
+    "url": "/wwwnice/static/js/main.a82f7ad6.chunk.js"
+  },
+  {
+    "revision": "cc360fb6938c4a946641",
+    "url": "/wwwnice/static/js/1.cc360fb6.chunk.js"
+  },
+  {
+    "revision": "199af2c2a0c941a3b7e7",
+    "url": "/wwwnice/static/js/2.199af2c2.chunk.js"
+  },
+  {
+    "revision": "8ac5f6939f6b1ad525b9",
+    "url": "/wwwnice/static/js/runtime~main.8ac5f693.js"
+  },
+  {
+    "revision": "5fb90a92f1b6c2ff135847bb81941a15",
+    "url": "/wwwnice/index.html"
+  }
+];
